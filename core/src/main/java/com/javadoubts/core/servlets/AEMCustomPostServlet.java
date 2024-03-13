a4737740ae31c4067c2af52ee4bac83c698fd05d
khanaaqib <35225284+khanaaqib@users.noreply.github.com>
2024-03-13 22:02:15 +0530

    Adding post servlet form changes

--- core/src/main/java/com/javadoubts/core/servlets/AEMCustomPostServlet.java
+++ core/src/main/java/com/javadoubts/core/servlets/AEMCustomPostServlet.java
@@ -1,12 +1,18 @@
 package com.javadoubts.core.servlets;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.apache.tika.parser.epub.EpubContentParser;
 import org.osgi.service.component.annotations.Component;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -15,8 +21,51 @@ import java.io.IOException;
         value="/bin/post/customServlet"
 )
 public class AEMCustomPostServlet extends SlingAllMethodsServlet {
+
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-         response.getWriter().write("hello world");
+        String nodeCreate= StringUtils.EMPTY;
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        Session session = resourceResolver.adaptTo(Session.class);
+        String nodeLocation = "/content/practice/aem-user-form";
+        try{
+            if(session.nodeExists(nodeLocation)){
+                nodeCreate = CreateUserSpecificNode(session,nodeLocation,request);
+            } else {
+               CreateUserFormNode(session);
+               nodeCreate = CreateUserSpecificNode(session,nodeLocation,request);
+            }
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        }
+        response.getWriter().write(nodeCreate);
+
+
+
+    }
+    public void CreateUserFormNode(Session session) throws RepositoryException {
+         if(session.nodeExists("/content/practice")){
+             Node praticeNode = session.getNode("/content/practice");
+             Node aemUserFormNode = praticeNode.addNode("aem-user-form","nt:unstructured");
+             session.save();
+         }
+    }
+
+    public String CreateUserSpecificNode(Session session , String nodeLocation, SlingHttpServletRequest request) throws RepositoryException {
+        String email = request.getParameter("Email");
+        Node userNode = session.getNode(nodeLocation);
+        if (userNode.hasNode(email)) {
+            return "user is present";
+        } else {
+            Node userSpecificNode = userNode.addNode(email, "nt:unstructured");
+            userSpecificNode.setProperty("firstName",request.getParameter("firstName"));
+            userSpecificNode.setProperty("lastName",request.getParameter("lastName"));
+            userSpecificNode.setProperty("email",request.getParameter("Email"));
+            userSpecificNode.setProperty("address",request.getParameter("Address"));
+            session.save();
+            return userSpecificNode.getName();
+        }
+
+
     }
 }
---
+++ ui.apps/src/main/content/jcr_root/apps/practice/components/aemUserForm/clientlib/js/aemuserform.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#user-custom-form").click(function(event){
+      event.preventDefault();
+      var firstName = $("#fname").val();
+      var lastName = $("#lname").val();
+      var email = $("#email").val();
+      var address = $("#address").val();
+       $.ajax({
+           url: "/bin/post/customServlet", 
+           type: 'POST',
+           data: {
+               firstName : firstName,
+               lastName : lastName,
+               Email: email,
+               Address: address
+           },
+           success: function(result){
+                alert("success");
+  		   },
+           error: function(error){
+               alert("failure");
+           }
+  });	
+  });
+});
