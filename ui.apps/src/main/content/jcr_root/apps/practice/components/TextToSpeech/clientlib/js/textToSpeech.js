88bb8061157360fb75508b4202248b666e97146b
khanaaqib <35225284+khanaaqib@users.noreply.github.com>
2024-05-04 16:00:10 +0530

    Added text to speech functionality

--- ui.apps/src/main/content/jcr_root/apps/practice/components/TextToSpeech/clientlib/js/textToSpeech.js
+++ ui.apps/src/main/content/jcr_root/apps/practice/components/TextToSpeech/clientlib/js/textToSpeech.js
@@ -1,34 +1,33 @@
+
 $(document).ready(function(){
         var form = document.getElementById('textToSpeechForm');
         var audioPlayer = document.getElementById('audioPlayer');
         form.addEventListener('submit', async function (event) {
             event.preventDefault();
-             var text = $("#textInput").val().trim();
-                     if (text) {
-                         var xhr = new XMLHttpRequest();
-                         xhr.open("POST", "/bin/ks/TextToSpeech", true); // Update the URL
-                         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                         xhr.responseType = "blob";
+             var text = $("#textInput").val().trim()
+             var myJson = '{\r\n    "lang": "english_us",\r\n "voice": "female_1"\r\n}';
+             var obj  = JSON.parse(myJson);
+             var newuser = "text";
+             obj[newuser] = text;
+            const settings = {
+                async: true,
+                crossDomain: true,
+                url: 'https://tts-tiktok.p.rapidapi.com/tts/v1/tts-tiktok-1?response=url',
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'X-RapidAPI-Key': '4ddf227a28mshf3d728c1b331629p138901jsnca8aa81bcbde',
+                    'X-RapidAPI-Host': 'tts-tiktok.p.rapidapi.com'
+                },
+                data: JSON.stringify(obj)
+            }
 
-                         xhr.onload = function() {
-                             if (xhr.status === 200) {
-                                 var blob = new Blob([xhr.response], { type: "audio/mpeg" });
-                                 var audioURL = URL.createObjectURL(blob);
-                                 audioPlayer.src = audioURL;
-                                 audioPlayer.play();
-                             } else {
-                                 console.error("Error:", xhr.statusText);
-                             }
-                         };
-
-                         xhr.onerror = function() {
-                             console.error("Request failed");
-                         };
-
-                         xhr.send("text=" + encodeURIComponent(text));
-                     } else {
-                         console.error("Text input is empty");
-                     }
+            $.ajax(settings).done(function (response) {
+                audioPlayer.src = response.data.url;
+                audioPlayer.play();
+                audioPlayer.style.display='block';
+                console.log(response);
+            });
 
         });
 
